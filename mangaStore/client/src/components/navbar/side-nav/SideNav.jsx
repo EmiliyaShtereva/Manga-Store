a78d9659484265703026acc23779b11488e4f634
EmiliyaShtereva <123276538+EmiliyaShtereva@users.noreply.github.com>
2023-11-15 19:07:32 +0200

    added genre click function for specific genres

--- mangaStore/client/src/components/navbar/side-nav/SideNav.jsx
+++ mangaStore/client/src/components/navbar/side-nav/SideNav.jsx
@@ -1,22 +1,43 @@
 import { useNavigate } from 'react-router-dom';
 import styles from './SideNav.module.css';
+import { useState } from 'react';
 
-export default function SideNav({ onClose}) {
+export default function SideNav({ onClose }) {
     const navigate = useNavigate();
+    const [showGenres, setShowGenres] = useState(false);
+
+    const onGenreClick = () => {
+        if (showGenres == false) {
+            setShowGenres(true);
+        } else {
+            setShowGenres(false);
+        }
+    }
 
     return (
         <div className={styles['sidenav-overlay']}>
             <div className={styles['sidenav-backdrop']} onClick={onClose}></div>
             <div className={styles['sidenav']}>
                 <button onClick={() => navigate('/home')}>Home</button>
-                <button>Genre</button>
+                <button onClick={onGenreClick} className={!showGenres ? '' : styles['active']}>Genre</button>
                 <button onClick={() => navigate('/newest')}>Newest</button>
-                <button onClick={() => navigate('/ongoing')}>Ongoin</button>
-                <button onClick={() => navigate('/completed')}>Completed</button>
+                <button onClick={() => navigate('/status/ongoing')}>Ongoin</button>
+                <button onClick={() => navigate('/status/completed')}>Completed</button>
                 <button onClick={() => navigate('/about')}>About</button>
-                <button onClick={() => navigate('/about')}>Contact</button>
+                <button onClick={() => navigate('/contact')}>Contact</button>
                 <button onClick={() => navigate('/questions')}>FAQ</button>
             </div>
+
+            {showGenres && (
+                <div className={styles['genre-sidenav']}>
+                    <button onClick={() => navigate('/genre/action')}>Action</button>
+                    <button onClick={() => navigate('/genre/horror')}>Horror</button>
+                    <button onClick={() => navigate('/genre/comedy')}>Comedy</button>
+                    <button onClick={() => navigate('/genre/romance')}>Romance</button>
+                    <button onClick={() => navigate('/genre/mystery')}>Mystery</button>
+                    <button onClick={() => navigate('/genre/sports')}>Sports</button>
+                </div>
+            )}
         </div>
     )
 }
