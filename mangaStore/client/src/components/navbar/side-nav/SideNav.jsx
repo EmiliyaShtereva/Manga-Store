ecd06488656aa12e1aec1a09464f39cf41464983
EmiliyaShtereva <123276538+EmiliyaShtereva@users.noreply.github.com>
2023-12-06 17:51:16 +0200

    added create to be shown only with authentication

--- mangaStore/client/src/components/navbar/side-nav/SideNav.jsx
+++ mangaStore/client/src/components/navbar/side-nav/SideNav.jsx
@@ -1,8 +1,11 @@
 import { useNavigate } from 'react-router-dom';
 import styles from './SideNav.module.css';
+import { useContext } from 'react';
+import AuthContext from '../../../context/authContext';
 
 export default function SideNav({ onClose }) {
     const navigate = useNavigate();
+    const { isAuthenticated } = useContext(AuthContext);
 
     return (
         <div className={styles['sidenav-overlay']}>
@@ -10,7 +13,7 @@ export default function SideNav({ onClose }) {
             <div className={styles['sidenav']}>
                 <button onClick={() => navigate('/')}>Home</button>
                 <button onClick={() => navigate('/catalog')}>Catalog</button>
-                <button onClick={() => navigate('/create')}>Create</button>
+                {isAuthenticated && (<button onClick={() => navigate('/create')}>Create</button>)}
                 <button onClick={() => navigate('/about')}>About</button>
                 <button onClick={() => navigate('/contact')}>Contact</button>
                 <button onClick={() => navigate('/questions')}>FAQ</button>
